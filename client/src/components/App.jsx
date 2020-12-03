1f2773922912672ab9ccffc68d9abf0596e13647
Sam Cox <sjamcox@gmail.com>
2020-12-03 12:10:47 -0800

    Create and add Stopwatch component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Stopwatch from './Stopwatch'
 
 const App = () => {
   return (
     <>
       <h1>Roast Tracker</h1>
+      <Stopwatch />
     </>
   )
 }
---
+++ client/src/components/Stopwatch.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+
+const Stopwatch = () => {
+
+  const [ startTime, setStartTime ] = useState(0);
+  const [ elapsed, setElapsed ] = useState(`0:00:00`);
+  const [ isRunning, setIsRunning ] = useState(false)
+
+  const start = () => {
+    setStartTime(Date.now())
+    setIsRunning(true)
+  }
+
+  const stop = () => {
+    setIsRunning(false)
+  }
+
+  const formatTime = (mil) => {
+    let decimal = Math.floor(mil / 10) % 100
+    let seconds = Math.floor(mil / 1000) % 60
+    let minutes = Math.floor(mil / 60000)
+    if (seconds < 10) {
+      seconds = `0${seconds}`
+    }
+    return `${minutes}:${seconds}:${decimal}`
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      setTimeout(() => {
+        setElapsed(formatTime(Date.now() - startTime))
+      }, 30)
+    }
+  }, [isRunning, elapsed])
+
+  return (
+    <div className='stopwatch'>
+      <h3>{elapsed}</h3>
+      <button onClick={() => start()}>Start</button>
+      <button onClick={() => stop()}>Stop</button>
+    </div>
+  )
+}
+
+export default Stopwatch
