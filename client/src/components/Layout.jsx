a97689d2d589285d622c417266a9dd13316ac565
Sam Cox <sjamcox@gmail.com>
2021-02-12 10:30:05 -0800

    Create Layout and Main components for consistent UI

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <Main>
+        { children }
+      </Main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Main = styled.main`
+  align-items: center;
+  display: flex;
+  height: calc(100vh - 144px);
+  justify-content: center;
+  padding: 48px 36px 36px 36px;
+  width: auto;
+`
+
+export default Main
