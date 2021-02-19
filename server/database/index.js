baf8282fbb6dc55438023766ee826db746cbfc9f
Sam Cox <sjamcox@gmail.com>
2021-02-18 19:44:48 -0800

    Remove dotenv from production

--- server/database/index.js
+++ server/database/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const mongoose = require('mongoose')
 const db = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
